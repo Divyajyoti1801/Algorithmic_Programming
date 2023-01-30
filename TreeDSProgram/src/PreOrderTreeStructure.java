569b597f28c33200b340eccb4f47fc4108560a0f
Divyajyoti <divyajyoti182000@gmail.com>
2023-01-30 18:10:30 +0530

    Basic binary tree and all traversals

---
+++ TreeDSProgram/src/PreOrderTreeStructure.java
@@ -0,0 +1,73 @@
+public class PreOrderTreeStructure {
+    // Node Creation
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Binary Tree Creation
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes); // Left Node
+            newNode.right = buildTree(nodes); // Right Node
+            return newNode;
+        }
+    }
+
+    // PreOrder Traversal
+    static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    // InOrder Traversal
+    static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    // PostOrder Traversal
+    static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = new int[] { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+    }
+}
